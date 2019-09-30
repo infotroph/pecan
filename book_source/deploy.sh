8ec3c3088d8a1f4ba3c1990b3c8a77388655efb3
Chris Black <chris@ckblack.org>
2019-09-30 12:15:51 +0100

    testing

--- book_source/deploy.sh
+++ book_source/deploy.sh
@@ -3,9 +3,15 @@
 #exit on error
 set -e
 
+SOURCE_COMMIT=`git log -n1 \
+  --format='%nBuilt from commit %h of PEcAn:%n%d %aI%n"%s"%n'`
+echo -e According to Git, this is ${SOURCE_COMMIT}
+echo -e According to Travis, this is ${TRAVIS_COMMIT} ${TRAVIS_COMMIT_MESSAGE}
+
+
 #check for environment variable
 if [ -z "${GITHUB_PAT}" ]; then
-    echo "GITHUB_PAT is not set. Not deploying."
+    echo "GITHUB_PAT is not set. Not uploading documentation."
     exit 0
 fi
 
@@ -14,7 +20,7 @@ echo "GITHUB_PAT variable made by Tony Gardella"
 
 # don't run on pull requests
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "TRAVIS_PULL_REQUEST is 'true'. Not building documentation."
+    echo "This is a pull request, so not uploading documentation until merged."
     exit 0
 fi
 
@@ -26,7 +32,7 @@ elif [ "$TRAVIS_BRANCH" = "develop" ]; then
 elif [ "$( echo $TRAVIS_BRANCH | sed -e 's#^release/.*$#release#')" = "release" ]; then
   VERSION="$( echo $TRAVIS_BRANCH | sed -e 's#^release/\(.*\)$#\1#' )"
 else
-  echo "Not Master, Develop, or Release Branch. Will not render Book."
+  echo "Not Master, Develop, or Release Branch. Will not upload documentation."
   exit 0
 fi
 
@@ -41,10 +47,12 @@ git config --global user.name "TRAVIS-DOC-BUILD"
 GH_STATUS=$(curl -s -w %{http_code} -I https://github.com/${GH_USER}/pecan-documentation -o /dev/null)
 if [[ $GH_STATUS != 200 ]]; then
   echo "Can't find a repository at https://github.com/${GH_USER}/pecan-documentation"
-  echo "Will not render Book."
+  echo "Will not upload documentation."
   exit 0
 fi
 
+SOURCE_COMMIT=`git log -n1 \
+  --format='%nBuilt from commit %h of PEcAn:%n%d %aI%n"%s"%n'`
 git clone https://${GITHUB_PAT}@github.com/${GH_USER}/pecan-documentation.git book_hosted
 cd book_hosted
 
@@ -58,5 +66,9 @@ rsync -a --delete ../_book/ $VERSION/
 
 # push updated documentation back up
 git add --all *
-git commit -m "Update the book `date`" || true
+# NB embedded newlines are intentional
+git commit -m "Update the book `date`
+
+  Compiled by Travis CI, build $TRAVIS_BUILD_NUMBER ($TRAVIS_BUILD_ID) job $TRAVIS_JOB_NUMBER ($TRAVIS_JOB_ID)
+  $SOURCE_COMMIT" || true
 git push -q origin master
